6271838dff8c41bb1536bb4118b1858bed5a4655
parkyodo <scarletdemon@naver.com>
2024-11-03 23:42:02 +0900

    feat: main - global pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // dto에 정의된 타입만 사용한다 (데이터 핸들링)
+      forbidNonWhitelisted: true, // dto에 정의되지 않은 타입은 에러를 반환
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
