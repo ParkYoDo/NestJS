0c298af089431fee3e176a0157816c5a6f2c1648
parkyodo <scarletdemon@naver.com>
2024-11-03 23:44:52 +0900

    feat: typeorm postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
 import { MovieModule } from './movie/movie.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
+import { Movie } from './movie/entities/movie.entity';
 
 @Module({
-  imports: [MovieModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [],
+      synchronize: true,
+    }),
+    MovieModule,
+  ],
 })
 export class AppModule {}
