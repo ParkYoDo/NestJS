61d075cff2ca4f56515eff08fe65e4290d683649
parkyodo <scarletdemon@naver.com>
2025-02-11 14:17:47 +0900

    feat: cache interceptor

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -1,3 +1,4 @@
+import { CacheInterceptor, CacheKey, CacheTTL } from '@nestjs/cache-manager';
 import {
   BadRequestException,
   Body,
@@ -33,6 +34,14 @@ export class MovieController {
   @Public()
   getMovies(@Query() dto: GetMovieDto, @UserId() userId?: number) {
     return this.movieService.findAll(dto, userId);
+  }
+
+  @Get('recent')
+  @UseInterceptors(CacheInterceptor)
+  @CacheKey('getMoviesRecent')
+  @CacheTTL(10 * 1000)
+  getMoviesRecent() {
+    return this.movieService.findRecent();
   }
 
   @Get(':id')
